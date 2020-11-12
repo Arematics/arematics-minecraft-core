fac6f9f88a0228e83fd1ef304fa9f18fd8b38c03
Enrico Messall <enrico.messall@arematics.com>
2020-11-12 10:43:42 +0100

    Settings Command

---
+++ McCore/src/main/java/com/arematics/minecraft/core/commands/SettingsCommand.java
@@ -0,0 +1,33 @@
+package com.arematics.minecraft.core.commands;
+
+import com.arematics.minecraft.core.command.CoreCommand;
+import com.arematics.minecraft.core.server.CorePlayer;
+import com.arematics.minecraft.data.service.InventoryService;
+import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.Inventory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SettingsCommand extends CoreCommand {
+
+    private final InventoryService service;
+
+    @Autowired
+    public SettingsCommand(InventoryService inventoryService) {
+        super("settings");
+        this.service = inventoryService;
+    }
+
+    @Override
+    protected boolean onDefaultCLI(CommandSender sender) {
+        return super.onDefaultCLI(sender);
+    }
+
+    @Override
+    protected boolean onDefaultUI(CorePlayer player) {
+        Inventory inv = service.getOrCreate("player.settings", "§6Settings", (byte) 45);
+        player.getPlayer().openInventory(inv);
+        return true;
+    }
+}
