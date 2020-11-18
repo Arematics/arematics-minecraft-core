f38777889beaf3827cb11d400543c69f66f31023
Enrico Messall <enrico.messall@arematics.com>
2020-11-18 23:09:04 +0100

    Updated SQL Files

--- global_data_source.sql
+++ global_data_source.sql
@@ -2,6 +2,21 @@ create schema if not exists soulpvp;
 
 use soulpvp;
 
+create table if not exists REVINFO
+(
+    rev int auto_increment
+        primary key,
+    revtstmp bigint null
+);
+
+create table if not exists ignored_player
+(
+    id bigint auto_increment
+        primary key,
+    ignorer varchar(36) not null,
+    ignored varchar(36) not null
+);
+
 create table if not exists permission
 (
     permission varchar(64) not null
@@ -9,11 +24,12 @@ create table if not exists permission
     description varchar(64) not null
 );
 
-create table if not exists permission_aud
+create table if not exists permission_AUD
 (
     permission varchar(64) not null,
     rev int not null,
     description varchar(120) null,
+    revtype tinyint null,
     primary key (permission, rev),
     constraint permission_aud_rev_uindex
         unique (rev)
@@ -36,12 +52,13 @@ create table if not exists ranks
     name varchar(16) not null,
     short_name varchar(8) not null,
     color_code varchar(16) not null,
-    last_change timestamp default current_timestamp() not null on update current_timestamp(),
+    in_team tinyint(1) not null,
+    last_change timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint ranks_name_uindex
         unique (name)
 );
 
-create table if not exists ranks_aud
+create table if not exists ranks_AUD
 (
     id bigint not null,
     rev int not null,
@@ -62,6 +79,7 @@ create table if not exists ranks_permission
     id bigint not null,
     permission varchar(64) not null,
     until timestamp null,
+    mode varchar(128) null,
     primary key (id, permission),
     constraint UK_ow1w1097pqv61rf5x3ky0o6lq
         unique (permission),
@@ -73,38 +91,26 @@ create table if not exists ranks_permission
             on update cascade on delete cascade
 );
 
-create table if not exists revinfo
-(
-    rev int auto_increment
-        primary key,
-    revtstmp bigint null
-);
-
 create table if not exists seasons
 (
-    name varchar(32) not null primary key,
-    season_from timestamp default current_timestamp() not null,
+    name varchar(32) not null primary key ,
+    season_from timestamp default CURRENT_TIMESTAMP not null,
     season_to timestamp default '2000-12-14 00:00:00' not null,
     constraint seasons_name_uindex
         unique (name)
-);
-
-create table if not exists test
-(
-    id bigint auto_increment
-        primary key,
-    test varchar(255) null
 );
 
 create table if not exists user
 (
     arematics_connection varchar(36) not null,
     uuid varchar(36) not null primary key,
+    last_name varchar(255) not null,
     last_join timestamp null,
     last_ip varchar(64) null,
     last_ip_change timestamp null,
     `rank` bigint default 1 not null,
     display_rank bigint null,
+    karma int not null,
     constraint user_arematics_connection_uindex
         unique (arematics_connection),
     constraint user_uuid_uindex
@@ -117,24 +123,13 @@ create table if not exists user
             on update cascade
 );
 
-create table if not exists user_configurations
-(
-    uuid varchar(36) not null,
-    name varchar(255) not null,
-    value varchar(255) not null,
-    primary key (uuid, name),
-    constraint user_configurations_user_uuid_fk
-        foreign key (uuid) references soulpvp.user (uuid)
-            on update cascade on delete cascade
-);
-
 create table if not exists bans
 (
     uuid varchar(36) not null
         primary key,
     banned_by varchar(36) not null,
     reason varchar(120) not null,
-    banned_until timestamp default current_timestamp() not null on update current_timestamp(),
+    banned_until timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint bans_banned_by_uuid_fk
         foreign key (banned_by) references user (uuid)
             on update cascade on delete cascade,
@@ -149,7 +144,7 @@ create table if not exists bans_history
         primary key,
     banned_by varchar(36) not null,
     reason varchar(120) not null,
-    banned_until timestamp default current_timestamp() not null on update current_timestamp(),
+    banned_until timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint bans_history_banned_by_uuid_fk
         foreign key (banned_by) references user (uuid)
             on update cascade,
@@ -158,19 +153,40 @@ create table if not exists bans_history
             on update cascade
 );
 
-create table if not exists user_aud
+create table if not exists user_AUD
 (
     arematics_connection varchar(36) not null,
     uuid varchar(36) not null,
     rev int not null,
+    last_name varchar(255) null,
     last_join timestamp null,
     last_ip varchar(64) null,
     last_ip_change timestamp null,
     `rank` bigint null,
     display_rank bigint null,
+    karma int not null,
+    revtype tinyint null,
     primary key (uuid, rev),
     constraint user_aud_rev_uindex
         unique (rev)
+);
+
+create table if not exists user_configurations
+(
+    uuid varchar(36) not null,
+    name varchar(255) not null,
+    value varchar(255) not null,
+    primary key (uuid, name),
+    constraint user_configurations_user_uuid_fk
+        foreign key (uuid) references user (uuid)
+            on update cascade on delete cascade
+);
+
+create table if not exists user_friends
+(
+    uuid varchar(36) not null,
+    target_uuid varchar(36) not null,
+    primary key (uuid, target_uuid)
 );
 
 create table if not exists user_permission
@@ -178,6 +194,7 @@ create table if not exists user_permission
     uuid varchar(36) not null,
     permission varchar(64) not null,
     until timestamp null,
+    mode varchar(128) null,
     primary key (uuid, permission),
     constraint UK_c0210mhu8ug1x46rokkiorusj
         unique (permission),
@@ -194,7 +211,7 @@ create table if not exists user_quests
     id bigint not null
         primary key,
     uuid varchar(36) not null,
-    end_time timestamp default current_timestamp() not null on update current_timestamp(),
+    end_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint user_quests_quests_id_fk
         foreign key (id) references quests (id)
             on update cascade on delete cascade,
@@ -209,7 +226,7 @@ create table if not exists warns
     warned_by varchar(36) not null,
     amount int default 1 not null,
     reason varchar(120) not null,
-    warned_time timestamp default current_timestamp() not null on update current_timestamp(),
+    warned_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint warns_user_uuid_fk
         foreign key (uuid) references user (uuid)
             on update cascade on delete cascade,
@@ -224,7 +241,7 @@ create table if not exists warns_history
     warned_by varchar(36) not null,
     amount int default 1 not null,
     reason varchar(120) not null,
-    warned_time timestamp default current_timestamp() not null on update current_timestamp(),
+    warned_time timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
     constraint warns_history_user_uuid_fk
         foreign key (uuid) references user (uuid)
             on update cascade on delete cascade,
@@ -232,8 +249,4 @@ create table if not exists warns_history
         foreign key (warned_by) references user (uuid)
             on update cascade on delete cascade
 );
-
-
-
-
 
---
+++ mode_data_source.sql
@@ -0,0 +1,101 @@
+create schema if not exists pvp;
+
+use pvp;
+
+create table if not exists clan
+(
+    id bigint auto_increment
+        primary key,
+    clan_name varchar(16) not null,
+    tag varchar(6) not null,
+    colorCode varchar(4) default '§b' not null,
+    kills int default 0 not null,
+    deaths int default 0 not null,
+    coins bigint default 0 not null,
+    constraint clan_clan_name_uindex
+        unique (clan_name),
+    constraint clan_tag_uindex
+        unique (tag)
+);
+
+create table if not exists clan_permission
+(
+    permission varchar(255) not null primary key,
+    constraint clan_permission_permission_uindex
+        unique (permission)
+);
+
+create table if not exists clan_ranks
+(
+    clan_id bigint not null,
+    rank_name varchar(16) not null,
+    colorCode varchar(4) not null,
+    rank_level int null,
+    primary key (clan_id, rank_name),
+    constraint clan_ranks__clan_fk
+        foreign key (clan_id) references clan (id)
+            on update cascade on delete cascade
+);
+
+create table if not exists clan_member
+(
+    clan_id bigint not null,
+    uuid varchar(36) not null,
+    clan_rank varchar(16) not null,
+    clan_kills int default 0 not null,
+    clan_deaths int default 0 not null,
+    primary key (uuid, clan_id),
+    constraint clan_member__rank_fk
+        foreign key (clan_id, clan_rank) references clan_ranks (clan_id, rank_name)
+            on update cascade on delete cascade
+);
+
+create table if not exists clan_rank_permission
+(
+    clan_id bigint not null,
+    rank_name varchar(16) not null,
+    permission varchar(255) not null,
+    primary key (clan_id, rank_name, permission),
+    constraint clan_rank_permission__rank_fk
+        foreign key (clan_id, rank_name) references clan_ranks (clan_id, rank_name)
+            on update cascade on delete cascade
+);
+
+create table if not exists cooldown
+(
+    `key` varchar(255) not null,
+    second_key varchar(255) not null,
+    end_time bigint not null,
+    primary key (`key`, second_key)
+);
+
+create table if not exists inventories
+(
+    id bigint auto_increment
+        primary key,
+    data_key varchar(128) not null,
+    title varchar(32) not null,
+    slots tinyint not null,
+    items longblob null
+);
+
+create table if not exists kit
+(
+    id bigint auto_increment
+        primary key,
+    name varchar(255) not null,
+    permission_permission varchar(255) null,
+    cooldown bigint not null,
+    min_play_time bigint null,
+    display_item longblob not null
+);
+
+create table if not exists player_game_stats
+(
+    uuid varchar(36) not null
+        primary key,
+    kills int default 0 null,
+    deaths int default 0 null,
+    bounty int null
+);
+
