02d475a3e5f1bdc2470f07b0ec3fd419be070bcc
Luca Korte <luca.lol.korte@googlemail.com>
2020-11-25 20:28:13 +0100

    Spawn Command teleport and set spawn

---
+++ McCore/src/main/java/com/arematics/minecraft/core/commands/SpawnCommand.java
@@ -0,0 +1,52 @@
+package com.arematics.minecraft.core.commands;
+
+import com.arematics.minecraft.core.annotations.Perm;
+import com.arematics.minecraft.core.annotations.SubCommand;
+import com.arematics.minecraft.core.command.CoreCommand;
+import com.arematics.minecraft.core.command.processor.parser.CommandProcessException;
+import com.arematics.minecraft.core.server.CorePlayer;
+import com.arematics.minecraft.data.mode.model.Warp;
+import com.arematics.minecraft.data.service.WarpService;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Perm(permission = "spawn", description = "teleports to spawn")
+public class SpawnCommand extends CoreCommand {
+
+    private final WarpCommand warpCommand;
+    private final WarpService warpService;
+
+
+    @Autowired
+    public SpawnCommand(WarpCommand warpCommand, WarpService warpService) {
+        super("spawn");
+        this.warpCommand = warpCommand;
+        this.warpService = warpService;
+    }
+
+    @Override
+    public void onDefaultExecute(CommandSender sender) {
+        if (!(sender instanceof Player))
+            throw new CommandProcessException("Only Player can perform this command");
+
+        CorePlayer player = CorePlayer.get((Player) sender);
+
+        try {
+            Warp warp = warpService.getWarp("spawn");
+            this.warpCommand.warpTo(player, warp);
+        } catch (Exception e) {
+            player.warn("Warp was not set yet").handle();
+        }
+
+    }
+
+    @SubCommand("set")
+    public void setSpawn(CorePlayer player) {
+
+        this.warpCommand.setWarpTo(player, "spawn");
+
+    }
+}
