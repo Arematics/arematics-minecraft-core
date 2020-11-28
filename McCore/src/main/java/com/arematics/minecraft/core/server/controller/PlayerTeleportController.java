1c8ebff7ebb013b2e5f93767f3bb1c99ae87c286
Luca Korte <luca.lol.korte@googlemail.com>
2020-11-28 16:31:57 +0100

    Bug-fix only one teleport request

--- McCore/src/main/java/com/arematics/minecraft/core/server/controller/PlayerTeleportController.java
+++ McCore/src/main/java/com/arematics/minecraft/core/server/controller/PlayerTeleportController.java
@@ -4,6 +4,7 @@ import com.arematics.minecraft.core.server.CorePlayer;
 import com.arematics.minecraft.core.utils.ArematicsExecutor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -14,7 +15,13 @@ public class PlayerTeleportController {
     // 1. who accepts, 2. who sends
     private final Set<PlayerTeleport> teleports = new HashSet<>();
 
-    public void sendTpaRequest(CorePlayer sender, CorePlayer receiver) { teleports.add(new PlayerTeleport(sender, receiver)); }
+    public void sendTpaRequest(CorePlayer sender, CorePlayer receiver) {
+        if(!teleports.contains(new PlayerTeleport(sender,receiver))) {
+            teleports.add(new PlayerTeleport(sender, receiver));
+        } else {
+            sender.warn("You have already sent the player a teleport request");
+        }
+    }
 
     public boolean accept(CorePlayer sender, CorePlayer receiver) {
         for(PlayerTeleport current : teleports) {
