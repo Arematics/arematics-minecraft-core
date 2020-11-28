1803343bed1bcc91538b72b95417ca99ba24bc53
Luca Korte <luca.lol.korte@googlemail.com>
2020-11-28 15:42:58 +0100

    Bug-fix Changed ArrayList to HashSet for performance

--- McCore/src/main/java/com/arematics/minecraft/core/server/controller/PlayerTeleportController.java
+++ McCore/src/main/java/com/arematics/minecraft/core/server/controller/PlayerTeleportController.java
@@ -4,25 +4,17 @@ import com.arematics.minecraft.core.server.CorePlayer;
 import com.arematics.minecraft.core.utils.ArematicsExecutor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class PlayerTeleportController {
 
     // 1. who accepts, 2. who sends
-    private final List<PlayerTeleport> teleports = new ArrayList<>();
+    private final Set<PlayerTeleport> teleports = new HashSet<>();
 
     public void sendTpaRequest(CorePlayer sender, CorePlayer receiver) { teleports.add(new PlayerTeleport(sender, receiver)); }
-
-    public List<PlayerTeleport> getRequest(CorePlayer receiver) {
-        return teleports;
-    }
 
     public boolean accept(CorePlayer sender, CorePlayer receiver) {
         for(PlayerTeleport current : teleports) {
