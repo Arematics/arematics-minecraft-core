cf0b85718db9ee469e7d426848bff16e920189c4
marvin.kokor <kokor@gmx.de>
2020-11-30 01:16:58 +0100

    add reply command

---
+++ McCore/src/main/java/com/arematics/minecraft/core/chat/Messenger.java
@@ -0,0 +1,37 @@
+package com.arematics.minecraft.core.chat;
+
+import com.arematics.minecraft.core.server.CorePlayer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+public class Messenger {
+
+    private final HashMap<CorePlayer, CorePlayer> lastMessenger = new HashMap<>();
+
+
+    public Messenger() {
+    }
+
+    public void sendMsg(CorePlayer player, CorePlayer target, String message) {
+        if(player == target) {
+            return;
+        }
+        lastMessenger.put(target, player);
+        message(player, target, message);
+    }
+
+    public void reply(CorePlayer player, String message) {
+        CorePlayer target = lastMessenger.get(player);
+        if(target != null) {
+           message(player, target, message);
+        }
+    }
+
+    private void message(CorePlayer player, CorePlayer target, String message) {
+        player.info("§8<§bMSG§8> §7Du §8» §b" + target.getPlayer().getDisplayName() + "§7: §f" + message).DEFAULT().disableServerPrefix().handle();
+        target.info("§8<§bMSG§8> §b" + player.getPlayer().getDisplayName() + " §8» §7Dir: §f" + message).DEFAULT().disableServerPrefix().handle();
+    }
+
+}
