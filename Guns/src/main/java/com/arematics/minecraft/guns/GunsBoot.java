2a08b5ef09396496910cab219045b8ce757c21bc
Enrico Messall <enrico.messall@arematics.com>
2020-12-03 16:03:40 +0100

    Guns Init Files

---
+++ Guns/src/main/java/com/arematics/minecraft/guns/GunsBoot.java
@@ -0,0 +1,20 @@
+package com.arematics.minecraft.guns;
+
+import com.arematics.minecraft.core.Bootstrap;
+
+public class GunsBoot extends Bootstrap {
+
+    public GunsBoot() {
+        super(false);
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
