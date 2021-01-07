776195db602c9d29fd577a55e1cfacfbee758d88
Enrico Messall <enrico.messall@arematics.com>
2021-01-07 08:46:49 +0100

    Afk Listener  + Added Enchanting Afk Close Listener  + Added Open Book Afk Close Listener

--- McCore/src/main/java/com/arematics/minecraft/core/listener/AfkCloseListener.java
+++ McCore/src/main/java/com/arematics/minecraft/core/listener/AfkCloseListener.java
@@ -1,22 +1,55 @@
 package com.arematics.minecraft.core.listener;
 
+import com.arematics.minecraft.core.items.CoreItem;
 import com.arematics.minecraft.core.server.CorePlayer;
 import com.arematics.minecraft.core.utils.ArematicsExecutor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class AfkCloseListener implements Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent event){
-        ArematicsExecutor.runAsync(() -> asyncUpdate(event));
+        ArematicsExecutor.runAsync(() -> asyncUpdateMove(event));
     }
 
-    private void asyncUpdate(PlayerMoveEvent event){
+    @EventHandler
+    public void onEnchant(EnchantItemEvent event){
+        CorePlayer player = CorePlayer.get(event.getEnchanter());
+        player.callAntiAfk();
+    }
+
+    @EventHandler
+    public void interactProcessor(PlayerInteractEvent event){
+        boolean bookOpen = openBookAntiAfk(event);
+    }
+
+    private boolean openBookAntiAfk(PlayerInteractEvent event){
+        CorePlayer player = CorePlayer.get(event.getPlayer());
+        CoreItem hand = player.getItemInHand();
+        if(isInteract(event) && hand != null && hand.getType() == Material.BOOK_AND_QUILL){
+            player.callAntiAfk();
+            ArematicsExecutor.asyncDelayed(player::callAntiAfk, 1, TimeUnit.MINUTES);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isInteract(PlayerInteractEvent event){
+        return event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK;
+    }
+
+    private void asyncUpdateMove(PlayerMoveEvent event){
         Location f = event.getFrom();
         Location t = event.getTo();
         if(f.getBlockX() != t.getBlockX() || f.getBlockY() != t.getBlockY() || f.getBlockZ() != t.getBlockZ()){
