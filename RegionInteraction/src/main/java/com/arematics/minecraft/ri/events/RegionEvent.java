4e8f19d7e83bf2ab059f82bd75c6a9c4edc237c8
Enrico Messall <enrico.messall@arematics.com>
2021-01-08 09:47:40 +0100

    Region Fix

--- RegionInteraction/src/main/java/com/arematics/minecraft/ri/events/RegionEvent.java
+++ RegionInteraction/src/main/java/com/arematics/minecraft/ri/events/RegionEvent.java
@@ -1,22 +1,18 @@
 package com.arematics.minecraft.ri.events;
 
+import com.arematics.minecraft.core.events.BaseEvent;
 import com.arematics.minecraft.core.server.CorePlayer;
 import com.arematics.minecraft.ri.MovementWay;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 @Getter
 @RequiredArgsConstructor
-public class RegionEvent extends Event {
+public class RegionEvent extends BaseEvent {
     private static final HandlerList handlerList = new HandlerList();
     private final ProtectedRegion region;
     private final MovementWay movement;
     private final CorePlayer player;
-
-    public HandlerList getHandlers() {
-        return handlerList;
-    }
 }
